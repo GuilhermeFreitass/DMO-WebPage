10ebc8f13e7a9bee5779195db0f287de81741f54
GuilhermeFreitass <guilhermeaqf@gmail.com>
2024-09-20 01:22:19 -0300

    update index.html

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,44 +1,56 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
+    this.handleClick = this.handleClick.bind(this);
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".lines",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  closeMenuOnClick() {
+    this.navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        this.navList.classList.remove(this.activeClass);
+        this.mobileMenu.classList.remove(this.activeClass);
+        this.animateLinks();
+      });
+    });
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+      this.closeMenuOnClick();  // Chama a função que fecha o menu ao clicar nos links
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".lines",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+
